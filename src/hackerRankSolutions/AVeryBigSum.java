729357dc2feda32bb46bf6308d1fc867c0770758
Omisandefunmi <omisandefunmi@gmail.com>
2022-08-03 18:05:29 +0100

    added ApplesAndOranges

--- src/hackerRankSolutions/AVeryBigSum.java
+++ src/hackerRankSolutions/AVeryBigSum.java
@@ -7,12 +7,7 @@ public class AVeryBigSum {
         int [] ar = new int []{1};
         System.out.println(++ar[0]);
     }
-    public static long aVeryBigSum(List <Integer> ar){
-        long result = 0;
-        for (int i = 0; i < ar.size() ; i++) {
-//           result=
-        }
-        return result;
-//        return ar.stream().reduce(0, ));
+    public static long aVeryBigSum(List <Long> ar){
+        return ar.stream().reduce(0L, Long::sum);
     }
 }
---
+++ src/hackerRankSolutions/ApplesAndOranges.java
@@ -0,0 +1,45 @@
+package hackerRankSolutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApplesAndOranges {
+    public static void main(String[] args) {
+        List<Integer> apples = new ArrayList<>();
+        List<Integer> oranges = new ArrayList<>();
+
+        apples.add(-2);
+        apples.add(2);
+        apples.add(1);
+
+        oranges.add(5);
+        oranges.add(-6);
+
+        int s = 7;
+        int t = 11;
+        int a = 5;
+        int b = 15;
+
+        countAppleAndOrange(s, t, a, b, apples, oranges);
+    }
+    public static void countAppleAndOrange(int s, int t, int a, int b, List<Integer> apples, List<Integer> oranges){
+        int countApples = 0;
+        int countOranges = 0;
+        for (Integer apple : apples) {
+            int appleFallArea = a + apple;
+            if (appleFallArea >= s && appleFallArea <= t) {
+                countApples++;
+            }
+        }
+
+        for (Integer orange : oranges) {
+            int orangeFallArea = b + orange;
+            if (orangeFallArea >= s && orangeFallArea <= t) {
+                countOranges++;
+            }
+        }
+
+        System.out.println(countApples);
+        System.out.println(countOranges);
+    }
+}
